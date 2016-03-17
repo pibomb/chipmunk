fa07ae24dbe780d49dc613f258db0bc2fe001217
pibomb <pibomb988@gmail.com>
2016-03-16 23:43:08 -0400

    Dot, Cross, and Distance
    Created the dot product, cross product, and distance formula for 2
    points.

--- chipmunk/geom.cpp
+++ chipmunk/geom.cpp
@@ -1,6 +1,12 @@
 #include "geom.h"
 
-int Add(int i1, int i2) {
-    return i1 - i2;
+#include <cmath>
+#include <utility>
+
+double chipmunk::dist(std::pair<double, double> p1, std::pair<double, double> p2) {
+    return sqrt(pow(p1.first-p2.first, 2) + pow(p1.second-p2.second, 2));
 }
 
+double chipmunk::dist(double x1, double y1, double x2, double y2) {
+	return sqrt(pow(x1-x2, 2) + pow(y1-y2, 2));
+}
--- test/main.cpp
+++ test/main.cpp
@@ -4,8 +4,21 @@
 
 using namespace std;
 
-int main()
-{
-    cout << Add(3, 5) << endl;
-    return 0;
+int main() {
+	cout << "Testing: geom" << endl;
+	cout << endl;
+	cout << "Dot Product" << endl;
+	cout << "(5, 3) * (7, 3) = " << chipmunk::dot(make_pair(5, 3), make_pair(7, 3)) << endl;
+	cout << "(5, 3) * (7, 3) = " << chipmunk::dot(5, 3, 7, 3) << endl;
+	cout << endl;
+	cout << "Cross Product" << endl;
+	cout << "(5, 3) x (7, 3) = " << chipmunk::cross(make_pair(5, 3), make_pair(7, 3)) << endl;
+	cout << "(5, 3) x (7, 3) = " << chipmunk::cross(5, 3, 7, 3) << endl;
+	cout << endl;
+	cout << "Distance" << endl;
+	cout << "dist(3, 4) = " << chipmunk::dist(3, 4) << endl;
+	cout << "dist((3, 4), (5, 2)) = " << chipmunk::dist(make_pair(3, 4), make_pair(5, 2)) << endl;
+	cout << "dist(3, 4, 5, 2) = " << chipmunk::dist(3, 4, 5, 2) << endl;
+
+	return 0;
 }
