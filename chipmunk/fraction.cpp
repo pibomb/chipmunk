fca8a5dabedcec8bde7a193534ca5db13d1209e5
FlamingPi <flamingdreadnought@gmail.com>
2016-04-07 00:00:46 -0400

    Added files via upload

---
+++ chipmunk/fraction.cpp
@@ -0,0 +1,83 @@
+#include "fraction.h"
+#include "algebra.h"
+#include <iostream>
+
+chipmunk::Fraction::Fraction(int num, int denom)
+{
+    numerator = num;
+    denominator = denom;
+}
+
+chipmunk::Fraction chipmunk::Fraction::add(chipmunk::Fraction other)
+{
+    int common = lcm(denominator, other.getDenom());
+    int f1 = common / denominator;
+    numerator *= f1;
+    denominator *= f1;
+    int f2 = common / other.getDenom();
+    other.numerator *= f2;
+    other.denominator *= f2;
+    int n = numerator + other.getNum();
+    simplify();
+    other.simplify();
+    chipmunk::Fraction frac(n, denominator);
+    frac.simplify();
+    return frac;
+}
+
+chipmunk::Fraction chipmunk::Fraction::sub(chipmunk::Fraction other)
+{
+    int common = lcm(denominator, other.getDenom());
+    int f1 = common / denominator;
+    numerator *= f1;
+    denominator *= f1;
+    int f2 = common / other.getDenom();
+    other.numerator *= f2;
+    other.denominator *= f2;
+    int n = numerator - other.getNum();
+    simplify();
+    other.simplify();
+    chipmunk::Fraction frac(n, denominator);
+    frac.simplify();
+    return frac;
+}
+
+chipmunk::Fraction chipmunk::Fraction::mult(chipmunk::Fraction other)
+{
+    int n = numerator * other.numerator;
+    int d = denominator * other.denominator;
+    chipmunk::Fraction frac(n, d);
+    frac.simplify();
+    return frac;
+}
+
+chipmunk::Fraction chipmunk::Fraction::div(chipmunk::Fraction other)
+{
+    int n = numerator * other.denominator;
+    int d = denominator * other.numerator;
+    chipmunk::Fraction frac(n, d);
+    frac.simplify();
+    return frac;
+}
+
+void chipmunk::Fraction::simplify()
+{
+    int common = chipmunk::gcd(numerator, denominator);
+    numerator /= common;
+    denominator /= common;
+}
+
+double chipmunk::Fraction::getValue()
+{
+    return (double)numerator / (double)denominator;
+}
+
+int chipmunk::Fraction::getNum()
+{
+return numerator;
+}
+
+int chipmunk::Fraction::getDenom()
+{
+return denominator;
+}
